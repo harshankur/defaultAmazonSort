1c839bec9669ba50fb9f807a92dc5bc1933e35ea
Harsh Ankur <harsh.ankur@outlook.com>
2019-12-06 18:51:23 +0900

    saving of default sort is possible now.

--- popup.js
+++ popup.js
@@ -8,17 +8,22 @@ let changeSort = document.getElementById('changeSort');
 let sortDictionary;
 
 chrome.storage.sync.get('order', function(data) {
-  sortDictionary = data.order;
-  chrome.storage.sync.get('sort', function(data) {
+sortDictionary = data.order;
+chrome.storage.sync.get('sort', function(data) {
     changeSort.getElementsByTagName('option')[sortDictionary[data.sort]].selected = 'selected';
-  });
+});
 })
 
-changeSort.onclick = function(element) {
-  // let color = element.target.value;
-  // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  //   chrome.tabs.executeScript(
-  //       tabs[0].id,
-  //       {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  // });
-};
+changeSort.addEventListener('change', function() {
+    chrome.storage.sync.set({sort: changeSort.options[changeSort.selectedIndex].value}, function() {
+        console.log(`Default sort order changed to ${changeSort.options[changeSort.selectedIndex].value}`);
+    })
+})
+
+// #TODO: Change script to changing sort orders
+// #TODO: Work on how to understand an amazon page after search is open
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+        tabs[0].id,
+        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+});
